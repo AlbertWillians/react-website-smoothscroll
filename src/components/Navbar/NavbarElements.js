c8e5de5c5abb11b4fcff99ea571fc3e25ecfb62f
Albert Willians <albertwillians@hotmail.com>
2021-11-22 13:39:00 -0300

    adicionando itens ao navBar

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -49,9 +49,10 @@ export const MobileIcon = styled.div`
       position: absolute;
       top: 0;
       right: 0;
-      transform: translate(-100%, 60%)
+      transform: translate(-100%, 60%);
       font-size: 1.8rem;
       cursor: pointer;
+      color: #fff;
    }
 `
 
@@ -82,5 +83,32 @@ export const NavLinks = styled(LinkS)`
 
    &.active {
       border-bottom: 3px solid #01bf71;
+   }
+`
+
+export const NavBtn = styled.nav`
+   display: flex;
+   align-items: center;
+
+   @media screen and (max-width: 768px) {
+      display: none;
+   }
+`
+
+export const NavBtnLink = styled(LinkR)`
+   border-radius: 50px;
+   background: #01bf71;
+   white-space: nowrap;
+   padding: 10px 22px;
+   color: #010606;
+   font-size: 16px;
+   outline: none;
+   border: none;
+   cursor: pointer;
+   transition: all 0.2s ease-in-out;
+   text-decoration: none;
+
+   &:hover {
+      background: #fff;
    }
 `
