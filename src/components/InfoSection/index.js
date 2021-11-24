5fddac4df709c0d8678e163ecb084299bc8ba55d
Albert Willians <albertwillians@hotmail.com>
2021-11-24 09:06:40 -0300

    Creating and formatting all the three sections os the webpage

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -26,7 +26,10 @@ const InfoSection = ({
    img,
    id,
    headline,
-   buttonLabel
+   buttonLabel,
+   primary,
+   dark,
+   dark2,
 }) => {
    return (
       <>
@@ -39,7 +42,18 @@ const InfoSection = ({
                         <Heading lightText={lightText}>{headline}</Heading>
                         <Subtitle darkText={darkText}>{description}</Subtitle>
                         <BtnWrap>
-                           <Button to='home' primary='true' dark='true' >{buttonLabel}</Button>
+                           <Button
+                              to='home'
+                              smooth={true}
+                              duration={500}
+                              spy={true}
+                              exact='true'
+                              offset={-80}
+                              primary={primary ? 1 : 0}
+                              dark={dark ? 1 : 0}
+                              dark2={dark2 ? 1 : 0}
+                           >{buttonLabel}
+                           </Button>
                         </BtnWrap>
                      </TextWrapper>
                   </Column1>
